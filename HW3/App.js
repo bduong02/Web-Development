354cf0dbbe5ec762a2a56b6ec8c5334df17a91d6
bduong02 <65621073+bduong02@users.noreply.github.com>
2024-05-20 02:12:48 -0700

    Update App.js

--- HW3/App.js
+++ HW3/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ChallengeListingScreen from './components/ChallengeListingScreen';
 import ChallengeCreationScreen from './components/ChallengeCreationScreen';
 import ChallengeRecordingScreen from './components/ChallengeRecordingScreen';
 
 const App = () => {
+    const [challenges, setChallenges] = useState([
+        {
+            id: 1,
+            name: 'Challenge 1',
+            segments: [{ name: 'Segment 1' }, { name: 'Segment 2' }],
+            bestRun: null // Initially no best run
+        },
+        // other challenges...
+    ]);
+
+    const updateBestRun = (challengeId, bestRun) => {
+        setChallenges(prevChallenges => prevChallenges.map(challenge =>
+            challenge.id === challengeId ? { ...challenge, bestRun } : challenge
+        ));
+    };
+
+    const addChallenge = (newChallenge) => {
+        setChallenges(prevChallenges => [...prevChallenges, newChallenge]);
+    };
+
     return (
         <Router>
             <div>
@@ -13,9 +33,9 @@ const App = () => {
                     <Link to="/create">Create Challenge</Link>
                 </nav>
                 <Routes>
-                    <Route path="/" element={<ChallengeListingScreen />} />
-                    <Route path="/create" element={<ChallengeCreationScreen />} />
-                    <Route path="/record/:id" element={<ChallengeRecordingScreen />} />
+                    <Route path="/" element={<ChallengeListingScreen challenges={challenges} />} />
+                    <Route path="/create" element={<ChallengeCreationScreen addChallenge={addChallenge} />} />
+                    <Route path="/record/:id" element={<ChallengeRecordingScreen challenges={challenges} updateBestRun={updateBestRun} />} />
                 </Routes>
             </div>
         </Router>
