00383b3ae1352d6fbbdbdfe88f86483d5ea47c33
bduong02 <65621073+bduong02@users.noreply.github.com>
2024-05-23 23:20:01 -0700

    Update main.js

--- Research Project/main.js
+++ Research Project/main.js
@@ -1,6 +1,6 @@
 // src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './components/router.js';
 
 createApp(App).use(router).mount('#app');
